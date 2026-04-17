b27aca5ba0844a277754c480dcdac304069317d7
Jeans <chalermpong.kvn@gmail.com>
2026-04-17 13:50:33 +0700

    Make property pages responsive

--- app/property/[propertyId]/page.tsx
+++ app/property/[propertyId]/page.tsx
@@ -18,14 +18,14 @@ export default async function Property({ params }: {
   const addressLines = [property.address1, property.address2, property.city, property.postcode].filter(addressLine => !!addressLine);
 
   return (
-    <div className="grid grid-cols-[1fr_500px]">
+    <div className="grid grid-cols-1 lg:grid-cols-[minmax(0,1fr)_420px] xl:grid-cols-[minmax(0,1fr)_500px]">
       <div>
         {!!property.images &&
           <Carousel className="w-full">
             <CarouselContent>
               {property.images.map((image, index) => (
                 <CarouselItem key={index}>
-                  <div className="relative h-[80vh] min-h-8">
+                  <div className="relative h-[42vh] min-h-64 sm:h-[55vh] lg:h-[80vh]">
                     <Image
                       src={`https://firebasestorage.googleapis.com/v0/b/fire-homes-5f6dd.firebasestorage.app/o/${encodeURIComponent(image)}?alt=media`}
                       alt={`image ${index + 1}`}
@@ -38,21 +38,21 @@ export default async function Property({ params }: {
             </CarouselContent>
             {property.images.length > 1 &&
               <>
-                <CarouselPrevious className="translate-x-24 size-12" />
-                <CarouselNext className="-translate-x-24 size-12" />
+                <CarouselPrevious className="left-4 size-10 sm:left-6 sm:size-12 lg:left-12" />
+                <CarouselNext className="right-4 size-10 sm:right-6 sm:size-12 lg:right-12" />
               </>
             }
           </Carousel>
         }
-        <div className="property-description max-w-screen-md mx-auto py-10 px-4">
+        <div className="property-description mx-auto max-w-screen-md px-4 py-8 sm:px-6 sm:py-10">
           <BackButton />
           <ReactMarkdown >{property.description}</ReactMarkdown>
         </div>
       </div>
-      <div className="bg-sky-200 p-10 h-screen sticky top-0 grid place-items-center">
-        <div className="flex flex-col gap-10 w-full">
+      <div className="bg-sky-200 px-4 py-6 sm:px-6 sm:py-8 lg:sticky lg:top-0 lg:h-screen lg:p-10 lg:grid lg:place-items-center">
+        <div className="flex w-full flex-col gap-6 sm:gap-8 lg:gap-10">
           <PropertyStatusBadge status={property.status} className="mr-auto text-base" />
-          <h1 className="text-4xl font-semibold">
+          <h1 className="text-3xl font-semibold leading-tight sm:text-4xl">
             {addressLines.map((addressLine, index) => (
               <div key={index}>
                 {addressLine}
@@ -60,14 +60,14 @@ export default async function Property({ params }: {
               </div>
             ))}
           </h1>
-          <h2 className="text-3xl font-light">
+          <h2 className="text-2xl font-light sm:text-3xl">
             ${numeral(property.price).format("0,0")}
           </h2>
-          <div className="flex gap-10">
-            <div className="flex gap-2">
+          <div className="flex flex-col gap-4 sm:flex-row sm:flex-wrap sm:gap-8">
+            <div className="flex items-center gap-2">
               <BedIcon /> {property.bedrooms} Bedrooms
             </div>
-            <div className="flex gap-2">
+            <div className="flex items-center gap-2">
               <BathIcon /> {property.bathrooms} Bathrooms
             </div>
           </div>
